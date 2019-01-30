ba8e1d4b24769843e33013881a4578f63deca908
Megan Dsouza <dsouza.me@northeastern.edu>
2019-01-30 12:17:30 -0800

    basic Auth

--- webapp/CloudApp/src/main/java/com/web/cloudapp/security/SpringSecurityConfig.java
+++ webapp/CloudApp/src/main/java/com/web/cloudapp/security/SpringSecurityConfig.java
@@ -7,20 +7,24 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.web.cloudapp.service.UserService;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private CustomAuthenticationProvider authProvider;
+    private UserService us;
 
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
-    }
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(us).passwordEncoder(new BCryptPasswordEncoder());
+}
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/user/register").permitAll()
                 .antMatchers("/").hasRole("ADMIN").and().httpBasic().and()
                 .csrf().disable().headers().frameOptions().disable();
