ed2d08fd6f2674f45c08d60e34cd640182ded8a9
vikram-ramesh <ramesh.vik@northeastern.edu>
2019-01-30 17:09:53 -0500

    Username Pwd validation

--- webapp/CloudApp/src/main/java/com/web/cloudapp/Controllers/loginController.java
+++ webapp/CloudApp/src/main/java/com/web/cloudapp/Controllers/loginController.java
@@ -46,6 +46,18 @@ public class loginController {
             map.put("status",HttpStatus.CONFLICT.toString());
             return new JSONObject(map).toString();
         }
+        else if(!userService.checkUserName(user.getUserName())) {
+            map.put("message","Please enter a valid email address");
+            map.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+            System.out.println("checkUserName");
+            return new JSONObject(map).toString();
+        }
+        else if(!userService.checkPassword(user.getPassword())) {
+            map.put("message","Please enter a password with atleast 1 Upper case, 1 Lower case, 1 Alphanumeric, 1 digit with minimum of 8 characters");
+            map.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+            System.out.println("checkPassword");
+            return new JSONObject(map).toString();
+        }
         else {
             String hashpass = hashpwd(user.getPassword());
             userRepository.save(new User(user.getUserName(), hashpass));
