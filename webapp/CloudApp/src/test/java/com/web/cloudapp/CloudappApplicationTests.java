68a30fc4bf3ea441f6aa4292aed141d401925aaf
bengre.t <bengre.t@husky.neu.edu>
2019-01-30 17:34:38 -0500

    test cases

--- webapp/CloudApp/src/test/java/com/web/cloudapp/CloudappApplicationTests.java
+++ webapp/CloudApp/src/test/java/com/web/cloudapp/CloudappApplicationTests.java
@@ -1,16 +1,25 @@
 package com.web.cloudapp;
 
+import com.web.cloudapp.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CloudappApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void checkPassword() {
+
+        CloudappApplicationTests cloudTests = new CloudappApplicationTests();
+        UserService userService = new UserService();
+        assertTrue(userService.checkPassword("Northeastern@Cloud67"));
+        assertTrue(userService.checkPassword("$Cloudcomputing2019"));
+
     }
 
 }
