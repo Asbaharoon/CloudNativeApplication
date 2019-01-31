25af5d25c45189d296ab96849b9d033850695a66
maruthi.v <maruthi.v@husky.neu.edu>
2019-01-31 12:30:30 -0500

    updated http codes

--- webapp/CloudApp/src/main/java/com/web/cloudapp/Controllers/loginController.java
+++ webapp/CloudApp/src/main/java/com/web/cloudapp/Controllers/loginController.java
@@ -38,25 +38,33 @@ public class loginController {
     }
 
     @RequestMapping(value= "/user/register", method= RequestMethod.POST)
-    public @ResponseBody String register(@RequestBody User user) {
+    public @ResponseBody ResponseEntity register(@RequestBody User user) {
         Map<String,String> map = new HashMap<String, String>();
         boolean userExists = findUser(user.getUserName());
         if(userExists){
             map.put("message","User Already Exists");
             map.put("status",HttpStatus.CONFLICT.toString());
-            return new JSONObject(map).toString();
+            //return new JSONObject(map).toString();
+            return new ResponseEntity(map, HttpStatus.CONFLICT);
+        }
+        else if(user.getUserName().isEmpty() || user.getPassword().isEmpty()){
+            map.put("message","Username/password is empty");
+            map.put("status", HttpStatus.BAD_REQUEST.toString());
+            return new ResponseEntity(map,HttpStatus.BAD_REQUEST);
         }
         else if(!userService.checkUserName(user.getUserName())) {
             map.put("message","Please enter a valid email address");
             map.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
             System.out.println("checkUserName");
-            return new JSONObject(map).toString();
+            //return new JSONObject(map).toString();
+            return new ResponseEntity(map, HttpStatus.NOT_ACCEPTABLE);
         }
         else if(!userService.checkPassword(user.getPassword())) {
             map.put("message","Please enter a password with atleast 1 Upper case, 1 Lower case, 1 Alphanumeric, 1 digit with minimum of 8 characters");
-            map.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+            map.put("status",HttpStatus.UNPROCESSABLE_ENTITY.toString());
             System.out.println("checkPassword");
-            return new JSONObject(map).toString();
+            //return new JSONObject(map).toString();
+            return new ResponseEntity(map, HttpStatus.UNPROCESSABLE_ENTITY);
         }
         else {
             String hashpass = hashpwd(user.getPassword());
@@ -65,7 +73,8 @@ public class loginController {
             map.put("message", "Registered Successfully");
             map.put("status", HttpStatus.OK.toString());
 
-            return new JSONObject(map).toString();
+            //return new JSONObject(map).toString();
+            return new ResponseEntity(map, HttpStatus.OK);
         }
     }
 
