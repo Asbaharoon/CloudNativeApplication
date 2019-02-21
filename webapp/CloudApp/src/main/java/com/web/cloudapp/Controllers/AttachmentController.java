0c5b0ab8abb5adf00a140dd9182d9e24910ab293
maruthi.v <maruthi.v@husky.neu.edu>
2019-02-20 22:42:57 -0500

    update and delete attachments

--- webapp/CloudApp/src/main/java/com/web/cloudapp/Controllers/AttachmentController.java
+++ webapp/CloudApp/src/main/java/com/web/cloudapp/Controllers/AttachmentController.java
@@ -94,4 +94,90 @@ public class AttachmentController {
         attachments = attachmentRepository.getAllAttachments(note);
         return new ResponseEntity(attachments,HttpStatus.OK);
     }
+
+    //delete
+    @Transactional
+    @RequestMapping(value="/note/{idNote}/attachments/{idAttachments}", method = RequestMethod.DELETE)
+    public  @ResponseBody ResponseEntity deleteNote(@PathVariable(value = "idNote") String idNote, @PathVariable(value = "idAttachments") String idAttachments){
+        Note note;
+        User user = service.getUserName();
+        Map<String,String> map = new HashMap<String, String>();
+        Attachment attachment;
+        attachment = attachmentRepository.getAttachment(idAttachments);
+        note = noteRepository.getNote(idNote);
+        if(note == null ){
+            map.put("status",HttpStatus.BAD_REQUEST.toString());
+            return new ResponseEntity(map,HttpStatus.BAD_REQUEST);
+        }
+        else if(!user.getUserName().equals(note.getUserData().getUserName())){
+            map.put("status",HttpStatus.UNAUTHORIZED.toString());
+            return new ResponseEntity(map,HttpStatus.UNAUTHORIZED);
+        }
+        else if(attachment == null){
+            map.put("status",HttpStatus.BAD_REQUEST.toString());
+            return new ResponseEntity(map,HttpStatus.BAD_REQUEST);
+        }
+        else {
+            try{
+                URL url = new URL(URLDecoder.decode( attachment.getUrl(), "UTF-8" ));
+                String path = url.getPath();
+                File file = new File(path);
+                file.delete();
+                System.out.println(attachment.getUrl());
+                attachmentRepository.delete(attachment);
+            }catch(Exception e){
+
+            }
+            map.put("status",HttpStatus.NO_CONTENT.toString());
+            map.put("message", "Deleted Successfully");
+            return new ResponseEntity(map,HttpStatus.NO_CONTENT);
+        }
+    }
+
+    //update
+    @PutMapping("/note/{idNote}/attachments/{idAttachment}")
+    public @ResponseBody ResponseEntity updateAttachment(@RequestParam("file") MultipartFile file, @PathVariable(value ="idNote") String idNote, @PathVariable(value = "idAttachment") String idAttachment) {
+        Note note;
+        User user = service.getUserName();
+        Map<String, String> map = new HashMap<String, String>();
+        Attachment attachment;
+        attachment = attachmentRepository.getAttachment(idAttachment);
+        note = noteRepository.getNote(idNote);
+        try {
+            URL url = new URL(URLDecoder.decode( attachment.getUrl(), "UTF-8" ));
+            String path1 = url.getPath();
+            File file1 = new File(path1);
+            file1.delete();
+
+            System.out.println(attachment.getUrl());
+
+            attachmentRepository.delete(attachment);
+            String pathDir = System.getProperty("user.home")+"/uploads/";
+            File pathNew = new File(pathDir);
+            if(!pathNew.exists()){
+                pathNew.mkdir();
+            }
+            File path = new File(pathDir+"_"+file.getOriginalFilename());
+            String attatchmentUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path(path.getAbsolutePath()).toUriString();
+            attachment.setUrl(attatchmentUrl);
+            attachmentRepository.save(attachment);
+            file.transferTo(path);
+        }
+
+        catch(Exception e){
+
+        }
+        if (note.getUserData() != user) {
+            map.put("status", HttpStatus.UNAUTHORIZED.toString());
+            return new ResponseEntity(map, HttpStatus.UNAUTHORIZED);
+        } else if (attachment == null) {
+            map.put("status", HttpStatus.BAD_REQUEST.toString());
+            return new ResponseEntity(map, HttpStatus.BAD_REQUEST);
+        } else {
+            map.put("status", HttpStatus.NO_CONTENT.toString());
+            return new ResponseEntity(map, HttpStatus.NO_CONTENT);
+        }
+    }
+
 }
