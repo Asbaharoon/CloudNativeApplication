f34807f641d17ad9f62ebf20d0cb96789f1ceb7e
Megan Dsouza <dsouza.me@northeastern.edu>
2019-03-14 08:57:57 -0700

    spring change

--- webapp/CloudApp/src/main/java/com/web/cloudapp/security/SpringSecurityConfig.java
+++ webapp/CloudApp/src/main/java/com/web/cloudapp/security/SpringSecurityConfig.java
@@ -26,7 +26,7 @@ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/user/register").permitAll()
-                .antMatchers("*/","*/note","*/note/*").hasRole("ADMIN").and().httpBasic().and()
+                .antMatchers("/CloudApp/","/CloudApp/note","CloudApp/note/*").hasRole("ADMIN").and().httpBasic().and()
                 .csrf().disable().headers().frameOptions().disable();
     }
 }
