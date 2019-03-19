3e944bd614b30771961f735550d116d2cd6c24cc
Megan Dsouza <dsouza.me@northeastern.edu>
2019-03-19 11:28:42 -0700

    Code Review Change

--- webapp/CloudApp/src/main/java/com/web/cloudapp/security/SpringSecurityConfig.java
+++ webapp/CloudApp/src/main/java/com/web/cloudapp/security/SpringSecurityConfig.java
@@ -26,7 +26,7 @@ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     protected void configure(HttpSecurity http) throws Exception {
        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/user/register").permitAll()
-                .antMatchers("/","/note","/note/*").hasRole("ADMIN").and().httpBasic().and()
+                .antMatchers("/","/notemegan","/notemegan/*").hasRole("ADMIN").and().httpBasic().and()
                 .csrf().disable().headers().frameOptions().disable();
     }
 }
