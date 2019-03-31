6736f7bbcb6123436abedd6f32ed53372eefd970
Vinyas Kaushik <vinyasktr@gmail.com>
2019-03-31 18:31:37 -0400

    auto-scaling cloudformation

--- infrastructure/aws/cloudformation/auto-scaling/csye6225-aws-cf-create-autoscaling-stack.sh
+++ infrastructure/aws/cloudformation/auto-scaling/csye6225-aws-cf-create-autoscaling-stack.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 echo "Creating Stack"
 stack_name=$1
-ec2InstanceTagVal=$2
+autoScaleGroupName="csye-6225-asg"
 DBUSER="csye6225master"
 DBPWD="csye6225password"
+topicName="password_reset"
+elb_security_group="elb-sg"
+
+
 
 if [ -z "$stack_name" ]; then
   echo "ERROR: Stackname expected....."
   exit 1
 fi
 
-if [ -z "$ec2InstanceTagVal" ]; then
+if [ -z "$autoScaleGroupName" ]; then
   echo "ERROR: ec2 Instance tag value expected....."
   exit 1
 fi
+
+CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[0].CertificateArn" --output text)
 
 keyTagValue=$(aws ec2 describe-key-pairs --query "KeyPairs[*].[KeyName]" --output text)
 
@@ -40,7 +46,7 @@ applicationName="csye6225-webapp"
 webSecurityGroupTagValue=csye6225-webapp
 dbSecurityGroupTagValue=csye6225-rds
 
-stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-application.json --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=amiId,ParameterValue=$amiId ParameterKey=ec2InstanceTag,ParameterValue=$ec2InstanceTagVal ParameterKey=DBUSER,ParameterValue=$DBUSER ParameterKey=DBPWD,ParameterValue=$DBPWD ParameterKey=accountID,ParameterValue=$account_id ParameterKey=awsRegion,ParameterValue=$region ParameterKey=s3CodeDeploy,ParameterValue=$s3codedeploy ParameterKey=s3Attachment,ParameterValue=$s3attachments ParameterKey=applicationName,ParameterValue=$applicationName ParameterKey=s3Lamba,ParameterValue=$s3lamba ParameterKey=lambaexecRole,ParameterValue=$lambaexecRole ParameterKey=domainName,ParameterValue=$domainName --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
+stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-application.json --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=amiId,ParameterValue=$amiId ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elbSecurityGroupNameTag ParameterKey=DBUSER,ParameterValue=$DBUSER ParameterKey=DBPWD,ParameterValue=$DBPWD ParameterKey=accountID,ParameterValue=$account_id ParameterKey=awsRegion,ParameterValue=$region ParameterKey=s3CodeDeploy,ParameterValue=$s3codedeploy ParameterKey=s3Attachment,ParameterValue=$s3attachments ParameterKey=applicationName,ParameterValue=$applicationName ParameterKey=s3Lamba,ParameterValue=$s3lamba ParameterKey=lambaexecRole,ParameterValue=$lambaexecRole ParameterKey=domainName,ParameterValue=$domainName ParameterKey=CertificateArn1,ParameterValue=$CERTIFICATE_ARN ParameterKey=topicName,ParameterValue=$topicName ParameterKey=hostedZoneName,ParameterValue=$domainName ParameterKey=autoScaleGroupName,ParameterValue=$autoScaleGroupName ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elb_security_group --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
 
 echo $stackId
 
