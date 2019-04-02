fdfcd5816dd239577d7d40c1a11aa04a821b3c78
Vinyas Kaushik <vinyasktr@gmail.com>
2019-04-02 00:21:07 -0400

    cloudFormation code changes

--- infrastructure/aws/cloudformation/auto-scaling/csye6225-aws-cf-create-autoscaling-stack.sh
+++ infrastructure/aws/cloudformation/auto-scaling/csye6225-aws-cf-create-autoscaling-stack.sh
@@ -6,7 +6,10 @@ DBUSER="csye6225master"
 DBPWD="csye6225password"
 topicName="password_reset"
 elb_security_group="elb-sg"
-
+region="us-east-1"
+applicationName="csye6225-webapp"
+webSecurityGroupTagValue=csye6225-webapp
+dbSecurityGroupTagValue=csye6225-rds
 
 
 if [ -z "$stack_name" ]; then
@@ -21,7 +24,11 @@ fi
 
 CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[0].CertificateArn" --output text)
 
+echo $CERTIFICATE_ARN
+
 keyTagValue=$(aws ec2 describe-key-pairs --query "KeyPairs[*].[KeyName]" --output text)
+
+echo $keyTagValue
 
 amiId=$(aws ec2 describe-images --filters "Name=tag:Name,Values=centos_assignment4" --query Images[0].ImageId --output text)
 
@@ -29,24 +36,24 @@ echo $amiId
 
 account_id=$(aws sts get-caller-identity --query "Account" --output text)
 
-region="us-east-1"
+echo $account_id
 
 s3codedeploy=$(aws s3api list-buckets --query "Buckets[*].[Name][0]" --output text)
 
+echo $s3codedeploy
+
 s3attachments=$(aws s3api list-buckets --query "Buckets[*].[Name][1]" --output text)
 
-s3lamba=$(aws s3api list-buckets --query "Buckets[*].[Name][2]" --output text)
+echo $s3attachments
 
-export lambaexecRole="arn:aws:iam::"$account_id":role/LambdaExecutionRole"
+
+# export lambdaexecRole="arn:aws:iam::"$account_id":role/LambdaExecutionRole"
 
 domainName=$(aws route53 list-hosted-zones --query "HostedZones[*].[Name]" --output text)
 
-applicationName="csye6225-webapp"
+echo $domainName
 
-webSecurityGroupTagValue=csye6225-webapp
-dbSecurityGroupTagValue=csye6225-rds
-
-stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-autoscaling.json --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=amiId,ParameterValue=$amiId ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elbSecurityGroupNameTag ParameterKey=DBUSER,ParameterValue=$DBUSER ParameterKey=DBPWD,ParameterValue=$DBPWD ParameterKey=accountID,ParameterValue=$account_id ParameterKey=awsRegion,ParameterValue=$region ParameterKey=s3CodeDeploy,ParameterValue=$s3codedeploy ParameterKey=s3Attachment,ParameterValue=$s3attachments ParameterKey=applicationName,ParameterValue=$applicationName ParameterKey=s3Lamba,ParameterValue=$s3lamba ParameterKey=lambaexecRole,ParameterValue=$lambaexecRole ParameterKey=domainName,ParameterValue=$domainName ParameterKey=CertificateArn1,ParameterValue=$CERTIFICATE_ARN ParameterKey=topicName,ParameterValue=$topicName ParameterKey=hostedZoneName,ParameterValue=$domainName ParameterKey=autoScaleGroupName,ParameterValue=$autoScaleGroupName ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elb_security_group --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
+stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-autoscaling.json --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=amiId,ParameterValue=$amiId ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elbSecurityGroupNameTag ParameterKey=DBUSER,ParameterValue=$DBUSER ParameterKey=DBPWD,ParameterValue=$DBPWD ParameterKey=accountID,ParameterValue=$account_id ParameterKey=awsRegion,ParameterValue=$region ParameterKey=s3CodeDeploy,ParameterValue=$s3codedeploy ParameterKey=s3Attachment,ParameterValue=$s3attachments ParameterKey=applicationName,ParameterValue=$applicationName ParameterKey=domainName,ParameterValue=$domainName ParameterKey=CertificateArn1,ParameterValue=$CERTIFICATE_ARN ParameterKey=topicName,ParameterValue=$topicName ParameterKey=hostedZoneName,ParameterValue=$domainName ParameterKey=autoScaleGroupName,ParameterValue=$autoScaleGroupName ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elb_security_group --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
 
 echo $stackId
 
