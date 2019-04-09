9f6fad9dd035c16012e86f5633e442ccf7bbe4c7
Vinyas Kaushik <vinyasktr@gmail.com>
2019-04-09 09:37:16 -0400

    new branch

--- infrastructure/aws/cloudformation/security-waf/csye6225-aws-cf-create-waf-stack.sh
+++ infrastructure/aws/cloudformation/security-waf/csye6225-aws-cf-create-waf-stack.sh
@@ -1,8 +1,31 @@
+#!/bin/bash
+echo "Starting waf creation"
+stack_name=$1
+
+if [ -z "$stack_name" ]; then
+  echo "ERROR: Stackname expected....."
+  exit 1
+fi
+
 LOADBALANCER=$(aws elbv2 describe-load-balancers --query LoadBalancers[0].LoadBalancerArn --output text)
+
+if [ -z "$LOADBALANCER" ]; then
+  echo "ERROR: LoadBalancer is not available."
+  exit 1
+fi
 
 echo "ELBResourceARN: $LOADBALANCER"
 
-echo "Starting waf creation"
-aws cloudformation create-stack --stack-name $1-waf --template-body file://./owasp_10_base.yml --parameters ParameterKey=LOADBALANCER,ParameterValue=$LOADBALANCER
-aws cloudformation wait stack-create-complete --stack-name $1-waf
-STACKDETAILS=$(aws cloudformation describe-stacks --stack-name $1-waf --query Stacks[0].StackId --output text)
+aws cloudformation create-stack --stack-name ${stack_name}-waf --template-body file://./../security-waf/owasp_10_base.yml --parameters ParameterKey=LOADBALANCER,ParameterValue=$LOADBALANCER
+aws cloudformation wait stack-create-complete --stack-name ${stack_name}-waf
+STACKDETAILS=$(aws cloudformation describe-stacks --stack-name ${stack_name}-waf --query Stacks[0].StackId --output text)
+echo $STACKDETAILS
+
+if [ -z $STACKDETAILS ]; then
+    echo 'Error occurred.Dont proceed. TERMINATED'
+    exit 1
+else
+    aws cloudformation wait stack-create-complete --stack-name ${stack_name}-waf
+    echo "STACK CREATION COMPLETE."
+fi
+exit 0
